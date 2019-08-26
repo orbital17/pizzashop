f5a5f811a23e4389553fb23691cce78f4109df54
tkachenko <olexiy.tkachenko@gmail.com>
2019-08-26 15:09:19 +0300

    add time to cook

--- cook/controller.js
+++ cook/controller.js
@@ -8,7 +8,11 @@ class Controller {
   async processOrder(msg) {
     const orderId = msg.content.toString()
     console.log(` received ${JSON.stringify(orderId)}`)
-    this.rabbitChannel.ack(msg)
+    const timeToCook = Math.floor(Math.random() * 4 + 1) // 1-5s
+    setTimeout(() => {
+      console.log(` done ${JSON.stringify(orderId)} in ${timeToCook}s`)
+      this.rabbitChannel.ack(msg)
+    }, timeToCook * 1000)
   }
 }
 
