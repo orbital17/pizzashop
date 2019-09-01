575a1f2c0afeaa7ee3207fc79c16ff1592900eb1
tkachenko <olexiy.tkachenko@gmail.com>
2019-09-01 18:30:35 +0300

    update tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 const test = require('ava')
 const { connect } = require('mongodb')
+const fetch = require('node-fetch')
 const { OrderStore } = require('../models/order')
 
 const mongoConfig = {
@@ -11,6 +12,7 @@ const mongoConfig = {
     useUnifiedTopology: true,
   },
 }
+const { WAITER_URL } = process.env
 
 test.before(async t => {
   const mongoClient = await connect(
@@ -31,8 +33,28 @@ test.after.always(async t => {
   await t.context.mongoClient.close()
 })
 
-test('get existing order', async t => {
-  const id = '5d63d991dee4aa40d7df2e63'
-  const doc = await t.context.orderStore.getById(id)
-  t.is(doc.status, 'ready')
+function newOrderRequest() {
+  return fetch(`${WAITER_URL}/order`, {
+    method: 'post',
+    body: JSON.stringify({
+      order: {
+        pizzas: [
+          {
+            ingridients: ['cheese', 'tomato'],
+          },
+        ],
+      },
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  }).then(res => res.json())
+}
+
+test.serial.only('creates new order', async t => {
+  const prevCount = await t.context.orderStore.collection.countDocuments()
+  const res = await newOrderRequest()
+  t.truthy(res.id)
+  const newCount = await t.context.orderStore.collection.countDocuments()
+  t.is(newCount, prevCount + 1)
+  const doc = await t.context.orderStore.getById(res.id)
+  t.true(doc.status == 'in_progress')
 })
